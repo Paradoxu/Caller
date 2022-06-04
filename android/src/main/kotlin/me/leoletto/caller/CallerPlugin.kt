3a810c6bf60781045221e690a351ec027fa662bb
= <leandroletto@gmail.com>
2022-06-04 10:57:01 +0200

    #7 - Fix override nullable parameters

--- android/src/main/kotlin/me/leoletto/caller/CallerPlugin.kt
+++ android/src/main/kotlin/me/leoletto/caller/CallerPlugin.kt
@@ -161,7 +161,7 @@ class CallerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegi
     currentActivity = null
   }
 
-  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>?, grantResults: IntArray?): Boolean {
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray): Boolean {
     return when (requestCode) {
       999 -> grantResults != null && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
       else -> false
